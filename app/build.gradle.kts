b6682c197c3cc609f6b2289d58b0ab32637308aa
Stefan Oltmann <github@stefan-oltmann.de>
2025-09-17 17:27:53 +0200

    Downgraded libraries due to compatibility issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -208,3 +208,12 @@ dependencies {
     macAarch64(compose.desktop.macos_arm64)
     windowsAmd64(compose.desktop.windows_x64)
 }
+
+// region Work around temporary Compose bugs.
+configurations.all {
+    attributes {
+        // https://github.com/JetBrains/compose-jb/issues/1404#issuecomment-1146894731
+        attribute(Attribute.of("ui", String::class.java), "awt")
+    }
+}
+// endregion
